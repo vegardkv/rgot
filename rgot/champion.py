8ae49480ad12076b231949b6a8afef81cf119bad
Knut Andreas Hasund <knutanha@gmail.com>
2015-11-22 13:41:53 +0100

    Added all keys to bonus_stats. Simplified adding item stats.

--- rgot/champion.py
+++ rgot/champion.py
@@ -6,8 +6,7 @@ class ItemSet:
     pass
 
 DamageSet = namedtuple('DamageSet', ['physical', 'magic', 'pure'])
-
-ATTACK_SPEED_NUMERATOR = 0.625 #  http://leagueoflegends.wikia.com/wiki/Attack_speed
+ATTACK_SPEED_NUMERATOR = 0.625  # http://leagueoflegends.wikia.com/wiki/Attack_speed
 
 class RuneSet:
     def __init__(self):
@@ -35,47 +34,150 @@ class Champion:
     def bonus_stats(self):
         if not self.__bonus_stats:
             # Populated by item ids of each unique item that has been processed in order to check for unique effects
-            unique_items = []
+            # unique_items = []
+
             # Matches item stat properties from "../data/all_items.json"
-            temp_bonus_stats = {"rFlatArmorPenetrationMod": 0.0,
-                                "rFlatPhysicalDamageModPerLevel": 0.0,
-                                "rPercentArmorPenetrationMod": 0.0,
-                                "FlatCritChanceMod": 0.0,
-                                "FlatCritDamageMod": 0.0,
-                                "FlatPhysicalDamageMod": 0.0,
-                                "PercentAttackSpeedMod": 0.0,
-                                "FlatArmorMod": 0.0}
+            temp_bonus_stats = {
+                # AD STATS
+                # Attack Damage
+                "rFlatPhysicalDamageModPerLevel": 0.0,
+                "FlatPhysicalDamageMod": 0.0,
+                "PercentPhysicalDamageMod": 0.0,
+                # Critical Chance
+                "FlatCritChanceMod": 0.0,
+                "FlatCritDamageMod": 0.0,
+                "PercentCritChanceMod": 0.0,    # Not used
+                "PercentCritDamageMod": 0.0,    # Not used
+                "rFlatCritChanceModPerLevel": 0.0,
+                "rFlatCritDamageModPerLevel": 0.0,
+                # Attack Speed
+                "PercentAttackSpeedMod": 0.0,
+                "FlatAttackSpeedMod": 0.0,  # Not used
+                "rPercentAttackSpeedModPerLevel": 0.0,
+                # Armor Penetration
+                "rFlatArmorPenetrationMod": 0.0,
+                "rPercentArmorPenetrationMod": 0.0,
+                "rFlatArmorPenetrationModPerLevel": 0.0,
+                "rPercentArmorPenetrationModPerLevel": 0.0,
+                # Life Steal
+                "PercentLifeStealMod": 0.0,
 
+                # AP STATS
+                # Ability Power / Magic Damage
+                "PercentMagicDamageMod": 0.0,
+                "FlatMagicDamageMod": 0.0,
+                "rFlatMagicDamageModPerLevel": 0.0,
+                # Magic Penetration
+                "rFlatMagicPenetrationMod": 0.0,
+                "rFlatMagicPenetrationModPerLevel": 0.0,
+                "rPercentMagicPenetrationMod": 0.0,
+                "rPercentMagicPenetrationModPerLevel": 0.0,
+                # Spell Vamp
+                "PercentSpellVampMod": 0.0,
 
-            # TODO: Add other stats than physical modifiers and armor
+                # COOLDOWN REDUCTION
+                "rPercentCooldownMod": 0.0,
+                "rPercentCooldownModPerLevel": 0.0,
+
+                # RESOURCE POOL STATS
+                # Mana
+                "FlatMPPoolMod": 0.0,
+                "rFlatMPModPerLevel": 0.0,
+                "PercentMPPoolMod": 0.0,
+                "FlatMPRegenMod": 0.0,
+                "rFlatMPRegenModPerLevel": 0.0,
+                "PercentMPRegenMod": 0.0,
+                # Energy
+                "FlatEnergyPoolMod": 0.0,
+                "rFlatEnergyModPerLevel": 0.0,
+                "FlatEnergyRegenMod": 0.0,
+                "rFlatEnergyRegenModPerLevel": 0.0,
+
+                # HP POOL
+                "FlatHPPoolMod": 0.0,
+                "rFlatHPModPerLevel": 0.0,
+                "PercentHPPoolMod": 0.0,
+                "FlatHPRegenMod": 0.0,
+                "rFlatHPRegenModPerLevel": 0.0,
+                "PercentHPRegenMod": 0.0,
+
+                # DEFENSIVE STATS
+                # Armor
+                "FlatArmorMod": 0.0,
+                "rFlatArmorModPerLevel": 0.0,
+                "PercentArmorMod": 0.0,
+                # Magic Resist / Spell Block
+                "FlatSpellBlockMod": 0.0,
+                "rFlatSpellBlockModPerLevel": 0.0,
+                "PercentSpellBlockMod": 0.0,
+                # Dodge (obsolete)
+                "rFlatDodgeMod": 0.0,
+                "rFlatDodgeModPerLevel": 0.0,
+                "PercentDodgeMod": 0.0,
+                # Block
+                "FlatBlockMod": 0.0,
+                "PercentBlockMod": 0.0,
+
+                # MOVEMENT SPEED
+                "FlatMovementSpeedMod": 0.0,
+                "rFlatMovementSpeedModPerLevel": 0.0,
+                "PercentMovementSpeedMod": 0.0,
+                "rPercentMovementSpeedModPerLevel": 0.0,
+
+                # MISC STATS
+                # Bonus XP
+                "FlatEXPBonus": 0.0,
+                "PercentEXPBonus": 0.0,
+                # Gold per 10 sec
+                "rFlatGoldPer10Mod": 0.0,
+                # Time Dead
+                "rFlatTimeDeadMod": 0.0,
+                "rFlatTimeDeadModPerLevel": 0.0,
+                "rPercentTimeDeadMod": 0.0,
+                "rPercentTimeDeadModPerLevel": 0.0,
+            }
+
+            # Collecting stats from items
+            # TODO: en UNIQUE handler (må tagges på en eller annen måte)
+            for item in self.__items:
+                for key, value in item["stats"].items():
+                    if key == "rPercentArmorPenetrationMod" or key == "FlatCritDamageMod":
+                        temp_bonus_stats[key] = max(value, temp_bonus_stats[key])
+                    else:
+                        temp_bonus_stats[key] += value
+            """
             for item in self.__items:
                 stats = item["stats"]
-                if "rFlatArmorPenetrationMod" in stats:
-                    temp_bonus_stats["rFlatArmorPenetrationMod"] += stats["rFlatArmorPenetrationMod"]
-
+                # Physical Damage
                 if "rFlatPhysicalDamageModPerLevel" in stats:
                     temp_bonus_stats["rFlatPhysicalDamageModPerLevel"] += stats["rFlatPhysicalDamageModPerLevel"]
+                if "FlatPhysicalDamageMod" in stats:
+                    temp_bonus_stats["FlatPhysicalDamageMod"] += stats["FlatPhysicalDamageMod"]
 
+                # Attack Speed
+                if "PercentAttackSpeedMod" in stats:
+                    temp_bonus_stats["PercentAttackSpeedMod"] += stats["PercentAttackSpeedMod"]
+
+                # Critical Chance/Damage
+                if "FlatCritChanceMod" in stats:
+                    temp_bonus_stats["FlatCritChanceMod"] += stats["FlatCritChanceMod"]
+                # Crit damage from items only given from Infinity Edge, which is an UNIQUE stat.
+                if "FlatCritDamageMod" in stats:
+                    temp_bonus_stats["FlatCritDamageMod"] = max(stats["FlatCritDamageMod"],
+                                                                temp_bonus_stats["FlatCritDamageMod"])
+                # Armor Penetration
+                if "rFlatArmorPenetrationMod" in stats:
+                    temp_bonus_stats["rFlatArmorPenetrationMod"] += stats["rFlatArmorPenetrationMod"]
                 # Percent armor penetration only occurs in unique effect "Last Whisper"
                 if "rPercentArmorPenetrationMod" in stats:
                     temp_bonus_stats["rPercentArmorPenetrationMod"] = max(
                         stats["rPercentArmorPenetrationMod"],
                         temp_bonus_stats["rPercentArmorPenetrationMod"])
 
-                if "FlatCritChanceMod" in stats:
-                    temp_bonus_stats["FlatCritChanceMod"] += stats["FlatCritChanceMod"]
-
-                if "FlatCritDamageMod" in stats:
-                    temp_bonus_stats["FlatCritDamageMod"] += stats["FlatCritDamageMod"]
-
-                if "FlatPhysicalDamageMod" in stats:
-                    temp_bonus_stats["FlatPhysicalDamageMod"] += stats["FlatPhysicalDamageMod"]
-
-                if "PercentAttackSpeedMod" in stats:
-                    temp_bonus_stats["PercentAttackSpeedMod"] += stats["PercentAttackSpeedMod"]
-
+                # Armor
                 if "FlatArmorMod" in stats:
                     temp_bonus_stats["FlatArmorMod"] += stats["FlatArmorMod"]
+                """
 
             self.__bonus_stats = temp_bonus_stats
 
